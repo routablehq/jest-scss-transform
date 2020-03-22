08ffaa3a210758285e75e464e398d609c09cccf4
Emily Kolar <emkolar@gmail.com>
2020-03-21 21:35:11 -0700

    (feature) add coverage report badges

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 module.exports = {
   automock: false,
+  coverageReporters: ['json-summary'],
   testMatch: [
     '<rootDir>/**/*.test.js'
   ],
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/example'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/example', '<rootDir>/dist'],
   transform: {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.scss$': path.resolve(__dirname, 'src/index.js')
