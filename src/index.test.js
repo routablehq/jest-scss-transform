5695bfc571f0bd747230a39d9a4d3b3ff5f8f7b2
Emily Kolar <emkolar@gmail.com>
2020-03-21 18:49:15 -0700

    (feature) add tests and supporting test files

---
+++ src/index.test.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const sinon = require('sinon');
+
+const transformer = require('./index');
+
+const basicFile = '../test/basic.scss';
+const commentsFile = '../test/comments.scss';
+const formattingFile = '../test/formatting.scss';
+const noExportsFile = '../test/none.scss';
+const incorrectExtFile = '../test/filename.css';
+
+const contentsBasic = fs.readFileSync(path.resolve(__dirname, basicFile), 'utf8');
+const contentsComments = fs.readFileSync(path.resolve(__dirname, commentsFile), 'utf8');
+const contentsFormatting = fs.readFileSync(path.resolve(__dirname, formattingFile), 'utf8');
+const contentsNoExports = fs.readFileSync(path.resolve(__dirname, noExportsFile), 'utf8');
+const contentsIncorrectExt = fs.readFileSync(path.resolve(__dirname, incorrectExtFile), 'utf8');
+
+const expectedParsed = 'module.exports = {"error":"$errorColor","success":"$successColor","warning":"$warningColor"};';
+const emptyModuleString = 'module.exports = {};';
+
+describe('jest-scss-transform', () => {
+  describe('transformer.process', () => {
+    it('returns empty module for file extensions other than .scss', () => {
+      expect(transformer.process(contentsIncorrectExt, incorrectExtFile)).toBe(emptyModuleString);
+    });
+
+    it('returns empty module for files with no exports', () => {
+      expect(transformer.process(contentsNoExports, noExportsFile)).toBe(emptyModuleString);
+    });
+
+    it('processes files with basic formatting', () => {
+      expect(transformer.process(contentsBasic, basicFile)).toBe(expectedParsed);
+    });
+
+    it('processes files with commented exports', () => {
+      expect(transformer.process(contentsComments, commentsFile)).toBe(expectedParsed);
+    });
+
+    it('processes files with poorly formatted exports', () => {
+      expect(transformer.process(contentsFormatting, formattingFile)).toBe(expectedParsed);
+    });
+  });
+
+  describe('transformer.getCacheKey', () => {
+    it('returns the expected cache key', () => {
+      const fsStub = sinon.stub(fs, 'readFileSync').returns('test contents');
+      const result = transformer.getCacheKey();
+      fsStub.restore();
+      expect(result).toBe('df14e44b311152c34358a675ae34afe0');
+    });
+  });
+});
